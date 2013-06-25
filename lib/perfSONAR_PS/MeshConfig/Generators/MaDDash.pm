1875427d4b4e2a3d46267c99a0b2a9e36bedda3f
alake <alake@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2013-06-25 14:31:47 +0000

    Reverting change to units I made recently. It was correct before, whoops.
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/trunk@5663 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

--- lib/perfSONAR_PS/MeshConfig/Generators/MaDDash.pm
+++ lib/perfSONAR_PS/MeshConfig/Generators/MaDDash.pm
@@ -434,9 +434,9 @@ sub __build_check {
     if ($type eq "perfsonarbuoy/bwctl") {
         my $ok_throughput = __get_check_option({ option => "acceptable_throughput", test_type => $type, maddash_options => $maddash_options });
         my $critical_throughput = __get_check_option({ option => "critical_throughput", test_type => $type, maddash_options => $maddash_options });
-        $check->{ok_description} = "Throughput >= ".$ok_throughput."Gbps";
-        $check->{warning_description} = "Throughput < ".$ok_throughput."Gbps";
-        $check->{critical_description} = "Throughput <= ".$critical_throughput."Gbps";
+        $check->{ok_description} = "Throughput >= ".$ok_throughput."Mbps";
+        $check->{warning_description} = "Throughput < ".$ok_throughput."Mbps";
+        $check->{critical_description} = "Throughput <= ".$critical_throughput."Mbps";
         $check->{params}->{graphUrl} = 'http://'.$host.'/serviceTest/bandwidthGraph.cgi?url=%maUrl&key=%maKeyF&keyR=%maKeyR&dstIP=%dstIP&srcIP=%srcIP&dst=%dstName&src=%srcName&type=TCP&length=2592000';
 
         # convert to Gbps used in the nagios plugin
