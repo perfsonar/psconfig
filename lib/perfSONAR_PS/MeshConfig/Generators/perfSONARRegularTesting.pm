cbf51e5543fed0be6f5e1b236e033a03718ca22d
aaron <aaron@6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b>
2014-02-06 13:49:12 +0000

    Copy over the Mesh Config from the regular testing branch
    git-svn-id: https://svn.internet2.edu/svn/perfSONAR-PS/branches/trunk-orig@5914 6dbdcc9b-cb27-0410-b2e0-cf3f5b33df3b

---
+++ lib/perfSONAR_PS/MeshConfig/Generators/perfSONARRegularTesting.pm
@@ -0,0 +1,369 @@
+package perfSONAR_PS::MeshConfig::Generators::perfSONARRegularTesting;
+use strict;
+use warnings;
+
+our $VERSION = 3.1;
+
+use Params::Validate qw(:all);
+use Log::Log4perl qw(get_logger);
+use Encode qw(encode);
+
+use utf8;
+
+use perfSONAR_PS::MeshConfig::Generators::Base;
+
+use perfSONAR_PS::RegularTesting::Utils::ConfigFile qw( parse_file save_string );
+
+use perfSONAR_PS::RegularTesting::Config;
+use perfSONAR_PS::RegularTesting::Test;
+use perfSONAR_PS::RegularTesting::Schedulers::Streaming;
+use perfSONAR_PS::RegularTesting::Tests::Powstream;
+use perfSONAR_PS::RegularTesting::Schedulers::RegularInterval;
+use perfSONAR_PS::RegularTesting::Tests::Bwctl;
+use perfSONAR_PS::RegularTesting::Tests::Bwtraceroute;
+use perfSONAR_PS::RegularTesting::Tests::Bwping;
+
+use Moose;
+
+extends 'perfSONAR_PS::MeshConfig::Generators::Base';
+
+has 'regular_testing_conf'   => (is => 'rw', isa => 'perfSONAR_PS::RegularTesting::Config');
+
+=head1 NAME
+
+perfSONAR_PS::MeshConfig::Generators::perfSONARRegularTesting;
+
+=head1 DESCRIPTION
+
+=head1 API
+
+=cut
+
+my $logger = get_logger(__PACKAGE__);
+
+sub init {
+    my ($self, @args) = @_;
+    my $parameters = validate( @args, { 
+                                         config_file     => 1,
+                                         skip_duplicates => 1,
+                                      });
+
+    my $config_file     = $parameters->{config_file};
+    my $skip_duplicates = $parameters->{skip_duplicates};
+
+    $self->SUPER::init({ config_file => $config_file, skip_duplicates => $skip_duplicates });
+
+    my $config;
+    eval {
+        my ($status, $res) = parse_file(file => $config_file);
+        if ($status != 0) {
+            $logger->error("Problem parsing configuration file: $res");
+            exit(-1);
+        }
+
+        my $conf = $res;
+
+        $config = perfSONAR_PS::RegularTesting::Config->parse($res);
+
+        # Remove the existing tests that were added by the mesh configuration
+        my @new_tests = ();
+        foreach my $test (@{ $config->tests }) {
+            next if ($test->added_by_mesh);
+
+            push @new_tests, $test;
+        }
+
+        $config->tests(\@new_tests);
+    };
+    if ($@) {
+        my $msg = "Problem initializing pinger landmarks: ".$@;
+        $logger->error($msg);
+        return (-1, $msg);
+    }
+
+    $self->regular_testing_conf($config);
+
+    return (0, "");
+}
+
+sub add_mesh_tests {
+    my ($self, @args) = @_;
+    my $parameters = validate( @args, { mesh => 1, tests => 1, host => 1 } );
+    my $mesh  = $parameters->{mesh};
+    my $tests = $parameters->{tests};
+    my $host  = $parameters->{host};
+
+    my %host_addresses = map { $_ => 1 } @{ $host->addresses };
+
+    my %addresses_added = ();
+
+    my $mesh_id = $mesh->description;
+    $mesh_id =~ s/[^A-Za-z0-9_-]/_/g;
+
+    my @tests = ();
+    foreach my $test (@$tests) {
+        if ($test->disabled) {
+            $logger->debug("Skipping disabled test: ".$test->description);
+            next;
+        }
+
+        $logger->debug("Adding: ".$test->description);
+
+        eval {
+            my %sender_targets = ();
+            my %receiver_targets = ();
+
+            foreach my $pair (@{ $test->members->source_destination_pairs }) {
+                my $matching_hosts = $mesh->lookup_hosts({ addresses => [ $pair->{destination}->{address} ] });
+                my $host_properties = $matching_hosts->[0];
+
+                my $sender = $pair->{"source"};
+                my $receiver = $pair->{"destination"};
+
+                # skip if we're not a sender or receiver.
+                next unless ($host_addresses{$sender->{address}} or $host_addresses{$receiver->{address}});
+
+                # skip if it's a loopback test.
+                next if ($host_addresses{$sender->{address}} and $host_addresses{$receiver->{address}});
+
+                # skip if we're 'no_agent'
+                next if (($host_addresses{$sender->{address}} and $sender->{no_agent}) or
+                           ($host_addresses{$receiver->{address}} and $receiver->{no_agent}));
+
+                if ($self->skip_duplicates) {
+                    # Check if a specific test (i.e. same
+                    # source/destination/test parameters) has been added
+                    # before, and if so, don't add it.
+                    my $already_added = $self->__add_test_if_not_added({ 
+                                                                         source             => $pair->{source}->{address},
+                                                                         destination        => $pair->{destination}->{address},
+                                                                         parameters         => $test->parameters,
+                                                                     });
+
+                    if ($already_added) {
+                        $logger->debug("Test between ".$pair->{source}->{address}." to ".$pair->{destination}->{address}." already exists. Not re-adding");
+                        next;
+                    }
+                }
+
+                if ($host_addresses{$sender->{address}}) {
+                    # We're the sender. We send in 3 cases:
+                    #   1) we're ping/traceroute, and there is no 'reverse' ping/traceroute test
+                    #   2) the far side is no_agent and won't be performing this test.
+                    #   3) the force_bidirectional flag is set so we perform both send and receive
+                    if ($receiver->{no_agent} or
+                        $test->parameters->type eq "traceroute" or
+                        $test->parameters->type eq "pinger" or
+                        ($test->parameters->can("force_bidirectional") and $test->parameters->force_bidirectional)) {
+
+                        $receiver_targets{$sender->{address}} = [] unless $receiver_targets{$sender->{address}};
+                        push @{ $receiver_targets{$sender->{address}} }, $receiver->{address};
+                    }
+                }
+                else {
+                    # we're the receiver. receiver always receives. Except traceroute, where we can't.
+                    if ($test->parameters->type eq "traceroute" or $test->parameters->type eq "pinger") {
+                        if ($sender->{no_agent}) {
+                            $logger->warn("Listed as a receiver for a test, but the far side isn't running an agent.");
+                        }
+                    }
+                    else {
+                        $sender_targets{$receiver->{address}} = [] unless $sender_targets{$receiver->{address}};
+                        push @{ $sender_targets{$receiver->{address}} }, $sender->{address};
+                    }
+                }
+            }
+    
+            # Produce a nicer looking config file if the sender and receiver set are the same
+            my $same_targets = 1;
+            foreach my $target (keys %receiver_targets) {
+                next if $sender_targets{$target};
+
+                $same_targets = 0;
+                last;
+            }
+
+            foreach my $target (keys %sender_targets) {
+                next if $receiver_targets{$target};
+
+                $same_targets = 0;
+                last;
+            }
+
+            if ($same_targets) {
+                my ($status, $res) = $self->__build_tests({ test => $test, targets => \%receiver_targets, target_receives => 1, target_sends => 1 });
+                if ($status != 0) {
+                    die("Problem creating tests: ".$res);
+                }
+
+                push @tests, @$res;
+            }
+            else {
+                my ($status, $res) = $self->__build_tests({ test => $test, targets => \%receiver_targets, target_receives => 1 });
+                if ($status != 0) {
+                    die("Problem creating tests: ".$res);
+                }
+
+                push @tests, @$res;
+
+                ($status, $res) = $self->__build_tests({ test => $test, targets => \%sender_targets, target_sends => 1 });
+                if ($status != 0) {
+                    die("Problem creating tests: ".$res);
+                }
+    
+                push @tests, @$res;
+            }
+
+        };
+        if ($@) {
+            die("Problem adding test ".$test->description.": ".$@);
+        }
+    }
+
+    push @{ $self->regular_testing_conf->tests }, @tests;
+
+    return;
+}
+
+sub __build_tests {
+    my ($self, @args) = @_;
+    my $parameters = validate( @args, { test => 1, targets => 1, target_sends => 0, target_receives => 0 });
+    my $test = $parameters->{test};
+    my $targets = $parameters->{targets};
+    my $target_sends = $parameters->{target_sends};
+    my $target_receives = $parameters->{target_receives};
+
+    my @tests = ();
+    foreach my $local_address (keys %{ $targets }) {
+        my $test_obj = perfSONAR_PS::RegularTesting::Test->new();
+        $test_obj->added_by_mesh(1);
+        $test_obj->description($test->description) if $test->description;
+        $test_obj->local_address($local_address);
+
+        my @targets = ();
+        foreach my $target (@{ $targets->{$local_address} }) {
+            my $target_obj = perfSONAR_PS::RegularTesting::Target->new();
+            $target_obj->address($target);
+            push @targets, $target_obj;
+        }
+        $test_obj->targets(\@targets);
+
+        my ($schedule, $parameters);
+
+        if ($test->parameters->type eq "pinger") {
+            $parameters = perfSONAR_PS::RegularTesting::Tests::Bwping->new();
+
+            $parameters->packet_count($test->parameters->packet_count) if $test->parameters->packet_count;
+            $parameters->packet_length($test->parameters->packet_size) if $test->parameters->packet_size;
+            $parameters->packet_ttl($test->parameters->packet_ttl) if $test->parameters->packet_ttl;
+            $parameters->inter_packet_time($test->parameters->packet_interval) if $test->parameters->packet_interval;
+            $parameters->force_ipv4($test->parameters->ipv4_only) if $test->parameters->ipv4_only;
+            $parameters->force_ipv6($test->parameters->ipv6_only) if $test->parameters->ipv6_only;
+
+            $schedule   = perfSONAR_PS::RegularTesting::Schedulers::RegularInterval->new();
+            $schedule->interval($test->parameters->test_interval);
+        }
+        elsif ($test->parameters->type eq "traceroute") {
+            $parameters = perfSONAR_PS::RegularTesting::Tests::Bwtraceroute->new();
+
+            $parameters->packet_length($test->parameters->packet_size) if $test->parameters->packet_size;
+            $parameters->packet_first_ttl($test->parameters->first_ttl) if $test->parameters->first_ttl;
+            $parameters->packet_max_ttl($test->parameters->max_ttl) if $test->parameters->max_ttl;
+            $parameters->force_ipv4($test->parameters->ipv4_only) if $test->parameters->ipv4_only;
+            $parameters->force_ipv6($test->parameters->ipv6_only) if $test->parameters->ipv6_only;
+
+            $schedule   = perfSONAR_PS::RegularTesting::Schedulers::RegularInterval->new();
+            $schedule->interval($test->parameters->test_interval) if $test->parameters->test_interval;
+        }
+        elsif ($test->parameters->type eq "perfsonarbuoy/bwctl") {
+            $parameters = perfSONAR_PS::RegularTesting::Tests::Bwctl->new();
+            $parameters->use_udp($test->parameters->protocol eq "udp"?1:0);
+            # $test{parameters}->{streams}  = $test->parameters->streams; # XXX: needs to support streams
+            $parameters->duration($test->parameters->duration) if $test->parameters->duration;
+            $parameters->udp_bandwidth($test->parameters->udp_bandwidth) if $test->parameters->udp_bandwidth;
+            $parameters->buffer_length($test->parameters->buffer_length) if $test->parameters->buffer_length;
+            $parameters->force_ipv4($test->parameters->ipv4_only) if $test->parameters->ipv4_only;
+            $parameters->force_ipv6($test->parameters->ipv6_only) if $test->parameters->ipv6_only;
+
+            $schedule   = perfSONAR_PS::RegularTesting::Schedulers::RegularInterval->new();
+            $schedule->interval($test->parameters->interval) if $test->parameters->interval;
+        }
+        elsif ($test->parameters->type eq "perfsonarbuoy/owamp") {
+            $parameters = perfSONAR_PS::RegularTesting::Tests::Powstream->new();
+            $parameters->resolution($test->parameters->sample_count * $test->parameters->packet_interval) if $test->parameters->sample_count * $test->parameters->packet_interval;
+            $parameters->inter_packet_time($test->parameters->packet_interval) if $test->parameters->packet_interval;
+            $parameters->force_ipv4($test->parameters->ipv4_only) if $test->parameters->ipv4_only;
+            $parameters->force_ipv6($test->parameters->ipv6_only) if $test->parameters->ipv6_only;
+
+            $schedule = perfSONAR_PS::RegularTesting::Schedulers::Streaming->new();
+        }
+
+        if ($target_sends and not $target_receives) {
+            $parameters->send_only(1);
+        }
+
+        if ($target_receives and not $target_sends) {
+            $parameters->receive_only(1);
+        }
+
+        $test_obj->parameters($parameters);
+        $test_obj->schedule($schedule);
+
+        push @tests, $test_obj;
+    }
+
+    return (0, \@tests);
+}
+
+sub get_config {
+    my ($self, @args) = @_;
+    my $parameters = validate( @args, { });
+
+    # Generate a Config::General version
+    my ($status, $res) = save_string(config => $self->regular_testing_conf->unparse());
+
+    return $res;
+}
+
+1;
+
+__END__
+
+=head1 SEE ALSO
+
+To join the 'perfSONAR Users' mailing list, please visit:
+
+  https://mail.internet2.edu/wws/info/perfsonar-user
+
+The perfSONAR-PS subversion repository is located at:
+
+  http://anonsvn.internet2.edu/svn/perfSONAR-PS/trunk
+
+Questions and comments can be directed to the author, or the mailing list.
+Bugs, feature requests, and improvements can be directed here:
+
+  http://code.google.com/p/perfsonar-ps/issues/list
+
+=head1 VERSION
+
+$Id: Base.pm 3658 2009-08-28 11:40:19Z aaron $
+
+=head1 AUTHOR
+
+Aaron Brown, aaron@internet2.edu
+
+=head1 LICENSE
+
+You should have received a copy of the Internet2 Intellectual Property Framework
+along with this software.  If not, see
+<http://www.internet2.edu/membership/ip.html>
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004-2009, Internet2 and the University of Delaware
+
+All rights reserved.
+
+=cut
+
+# vim: expandtab shiftwidth=4 tabstop=4
