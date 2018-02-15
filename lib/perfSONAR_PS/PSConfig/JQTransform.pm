75f01a52ed38bb115d3f0c7bcbb33071f26a90d5
Andy Lake <andy@es.net>
2018-02-15 11:57:52 -0500

    More packaging clean-up. Removing import that add unnecessary includes to package. Also made default maddash-agent.json valid.

--- lib/perfSONAR_PS/PSConfig/JQTransform.pm
+++ lib/perfSONAR_PS/PSConfig/JQTransform.pm
@@ -1,8 +1,8 @@
 package perfSONAR_PS::PSConfig::JQTransform;
 
 use Mouse;
+use JSON;
 use JSON::Validator;
-use perfSONAR_PS::PSConfig::PScheduler::Schema qw( psconfig_pscheduler_json_schema );
 use perfSONAR_PS::Utils::JQ qw( jq );
 
 extends 'perfSONAR_PS::Client::PSConfig::BaseNode';
@@ -53,21 +53,34 @@ Validates this object against JSON schema. Returns any errors found. Valid if li
 sub validate {
     my $self = shift;
     my $validator = new JSON::Validator();
-    my $schema = psconfig_pscheduler_json_schema();
     #tweak it so we just look at JQTransformSpecification
-    $schema->{'properties'} = {
-        'transform' => { 
-            '$ref' => '#/pSConfig/JQTransformSpecification',
-            'description' => 'JQ script to transform downloaded pSConfig JSON'
-        }
-    };
-    $schema->{'required'} = [ 'transform' ];
-    $validator->schema($schema);
+    $validator->schema(_schema());
     
     #plug-in archive in a way that will validate
-    return $validator->validate({'transform' =>  $self->data()});
+    return $validator->validate($self->data());
 }
 
+sub _schema() {
+
+    my $raw_json = <<'EOF';
+{
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "type": "object",
+    "properties": {
+        "script":   {
+            "anyOf": [
+                { "type": "string" },
+                { "type": "array", "items": { "type": "string" } }
+            ]
+        }
+    },
+    "additionalProperties": false,
+    "required": [ "script" ]
+}   
+EOF
+
+    return from_json($raw_json);
+}
 
 __PACKAGE__->meta->make_immutable;
 
